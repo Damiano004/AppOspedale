b83ecc84b41daabf37c5e7ccd44288ad8fd66cb6
Damiano004 <damiano.purin@mat.tn.it>
2025-06-04 23:36:34 +0200

    aggiunte le icone di primeNG

--- AFPHospitalSIOV2/src/app/feature/lista-pz/lista-pz.component.ts
+++ AFPHospitalSIOV2/src/app/feature/lista-pz/lista-pz.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { AFPHospitalAPIService } from '../../core/services/afphospital-api.service';
 import {RouterLink} from '@angular/router';
 import { CardComponent } from "../../UI/card/card.component";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-lista-pz',
-  imports: [RouterLink, CardComponent],
+  imports: [RouterLink, CardComponent,ButtonModule],
   templateUrl: './lista-pz.component.html',
   styleUrl: './lista-pz.component.scss'
 })
