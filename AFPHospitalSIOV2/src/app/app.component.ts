d1b8219a385c57ef825d6e79ac4e23585b63d2ee
Damiano004 <damiano.purin@mat.tn.it>
2025-06-05 22:03:23 +0200

    messa la select all'interno della navbar, molto meglio

--- AFPHospitalSIOV2/src/app/app.component.ts
+++ AFPHospitalSIOV2/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { AFPHospitalAPIService } from './core/services/afphospital-api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, SelectModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly api = inject(AFPHospitalAPIService);
+}
